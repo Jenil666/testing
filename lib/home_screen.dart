686d6f24f3449348e04ce335baa193a05987ca22
Jenil Chandarana <116253963+Jenil666@users.noreply.github.com>
2024-03-04 22:39:17 +0530

    code refectored

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,22 +12,59 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        commonTextField(
-          controller: TextEditingController(),
-          hintText: "Hi Corat",
-          titleText: "Required Field",
-          isRequired: true,
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              commonTextField(
+                controller: TextEditingController(),
+                hintText: "Hi Corat",
+                titleText: "Required Field",
+                isRequired: true,
+                validator: (p0) {
+                  if (p0!.isEmpty) {
+                    return "This field is required";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              commonTextField(
+                controller: TextEditingController(),
+                hintText: "Hi Corat",
+                titleText: "Not Required Field",
+                isRequired: false,
+                validator: (p0) {
+                  if (p0!.isEmpty) {
+                    return "This field is required";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 10),
+              commonTextField(
+                controller: TextEditingController(),
+                hintText: "Hi Corat",
+                titleText: "Number validation",
+                isRequired: true,
+                keyboardType: TextInputType.numberWithOptions(),
+                validator: (p0) {
+                  if (p0!.isEmpty) {
+                    return "This field is required";
+                  } else if (p0.contains("-") ||
+                      p0.contains(",") ||
+                      p0.contains(".")) {
+                    return "Enter valid Value";
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
         ),
-        SizedBox(height: 10),
-        commonTextField(
-          controller: TextEditingController(),
-          hintText: "Hi Corat",
-          titleText: "Not Required Field",
-          isRequired: false,
-        ),
-      ],
+      ),
     );
   }
 
@@ -35,7 +72,9 @@ class _HomeScreenState extends State<HomeScreen> {
     required TextEditingController controller,
     required String hintText,
     required String titleText,
+    String? Function(String?)? validator,
     bool? isRequired,
+    TextInputType? keyboardType,
   }) {
     return Column(
       children: [
@@ -43,8 +82,12 @@ class _HomeScreenState extends State<HomeScreen> {
         SizedBox(height: 5),
         TextFormField(
           controller: controller,
+          validator: validator,
+          keyboardType: keyboardType ?? TextInputType.text,
           autovalidateMode: AutovalidateMode.onUserInteraction,
-          decoration: const InputDecoration(
+          decoration: InputDecoration(
+            hintStyle: TextStyle(color: Colors.black26),
+            hintText: hintText,
             disabledBorder: OutlineInputBorder(
               borderSide: BorderSide(color: Colors.black12),
             ),
@@ -77,9 +120,13 @@ class _HomeScreenState extends State<HomeScreen> {
           textAlign: TextAlign.left,
           style: const TextStyle(color: Colors.black, fontSize: 15),
         ),
-        const Text(
-          "*",
-          style: TextStyle(color: Colors.red, fontSize: 15),
+        SizedBox(width: 5),
+        Visibility(
+          visible: isRequired,
+          child: const Text(
+            "*",
+            style: TextStyle(color: Colors.red, fontSize: 20),
+          ),
         ),
       ],
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:testing/home_screen.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {},
+      home: HomeScreen(),
     ),
   );
 }
